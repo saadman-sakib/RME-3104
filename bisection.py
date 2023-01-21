ba0711a8405e0b207b3a9ae377ce3606246ba57c
Saadman Sakib <saadman.sakib2020@gmail.com>
2023-01-22 03:34:17 +0600

    lecture 1 assignment

--- bisection.py
+++ bisection.py
@@ -1,7 +1,9 @@
+THRESHOLD = .00000000000000001
+
 def bisect(function, lower, upper):
     l, r, f = lower, upper, function
     m = 0
-    while abs(f(l)-0) > .00000000000000001:
+    while abs(f(l)-0) > THRESHOLD:
         m = (l+r)/2
         if f(m) > 0 :
             r = m
--- false_position.py
+++ false_position.py
@@ -1,7 +1,10 @@
+THRESHOLD = .00000000000000001
+
+
 def false_position(function, lower, upper):
     l, r, f = lower, upper, function
     m = 0
-    while abs(f(l)-0) > .00000000000000000001:
+    while abs(f(l)-0) > THRESHOLD:
         w1, w2 = abs(1/f(l)), abs(1/f(r))
         m = (w1*l + w2*r)/(w1 + w2)
         if f(m) > 0 :
