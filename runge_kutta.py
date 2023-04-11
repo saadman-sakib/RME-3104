bce2a307c3f1c3aced01d49dbf1a9d3963e74b63
Saadman Sakib <saadman.sakib2020@gmail.com>
2023-04-11 21:58:04 +0600

    runge kutta

--- runge_kutta.py
+++ runge_kutta.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
 
 def dy_dx(x, y):
-    return y + x
+    return y**(.5) + x
 
 x_0, y_0 = 0, 0
-
-
 points = [(x_0, y_0)]
 
-dx = .01
+h = .01
 
 x, y = x_0, y_0
 for i in range(1000):
-    x = x+dx
-    y = y+dy_dx(x,y)*dx
+    k1 = dy_dx(x, y)
+    k2 = dy_dx(x+h/2, y+k1*h/2)
+    k3 = dy_dx(x+h/2, y+k2*h/2)
+    k4 = dy_dx(x+h, y+k3*h)
+    y = y + (k1 + 2*k2 + 2*k3 + k4)*h/6
+    x = x+h
     points.append((x,y))
 
 
